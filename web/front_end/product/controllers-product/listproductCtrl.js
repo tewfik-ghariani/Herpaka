69c9639f3d036a4198abcf8f7560da5729ad9546
oumaima <manari.oumaima@gmail.com>
2017-01-15 21:53:09 +0100

    integrating views  to index.html + adding the missing frontend dependencies in bower.json + fixing the requirements of ng-template to activate routing + adjusting ng-class in the product variation according to its polarity (2 new CSS classes: greenclass and redclass)+ modifying the controller of list product to display products.data

--- web/front_end/product/controllers-product/listproductCtrl.js
+++ web/front_end/product/controllers-product/listproductCtrl.js
@@ -3,13 +3,26 @@ var app = angular.module('app');
 app.controller('listproductCtrl', ['$scope', 'providerFactory', 
   function ($scope, providerFactory) {
 
-    	 	console.log(providerFactory);
-  	$scope.products = providerFactory.fetchProducts().then(function(response){
+  	$scope.provider = providerFactory.fetchProducts().then(function(response){
 
-  		$scope.products = response.data;
+  		$scope.provider = response.data;
+  		if ($scope.provider.success) 
+  		{
+  			$scope.products =$scope.provider.data;
+  		}
   	});
 
+  	$scope.checkVariation = function(variation){
 
+  		if (variation >0)
+  		{
+  			return "redclass";
+  		}
+  		else {
+  			return "greenclass";
+  			
+  		}
+  	}
 
    
   }]);
