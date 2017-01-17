8646315d5916826c75a7a33506e9831439292282
HamedSaidi <hamedessaidi@gmail.com>
2017-01-17 16:36:00 +0100

    deactivating csrf_tokrn

--- web/main_js/services.js
+++ web/main_js/services.js
@@ -49,3 +49,17 @@ app.factory('providerFactory', ['$http', function($http) {
 
 
 }]);
+
+
+angular.module('cacheExampleApp', []).
+controller('CacheController', ['$scope', '$cacheFactory', function($scope, $cacheFactory) {
+  $scope.keys = [];
+  $scope.cache = $cacheFactory('cacheId');
+  $scope.put = function(key, value) {
+    if (angular.isUndefined($scope.cache.get(key))) {
+      $scope.keys.push(key);
+    }
+    $scope.cache.put(key, angular.isUndefined(value) ? null : value);
+  };
+}]);
+
