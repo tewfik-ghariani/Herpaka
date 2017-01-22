434edf966572ed5f1c18533961001d276a861126
Yosr <yosr.chebbi@ymail.com>
2017-01-22 14:50:29 +0100

    Fixing login and register views

--- src/ProductBundle/Document/Product.php
+++ src/ProductBundle/Document/Product.php
@@ -233,7 +233,7 @@ class Product
     public function showProduct()
     {
         $product = array(
-                         "id"=>$this->swid,
+                         "id"=>$this->sid,
                          "ProductName"=>$this->productName,
                          "Category"=>$this->category,
                          "Brand"=>$this->brand,
